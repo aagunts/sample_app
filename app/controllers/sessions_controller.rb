98c88e828980276766fc7dbc5ab3178558c53c80
kyamamoto <aagunts@gmail.com>
2016-03-01 11:01:46 +0000

    8.29

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,14 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
   
 end
