741ceeea14e51331058c4c8ad403d8e7e89d33ae
kyamamoto <aagunts@gmail.com>
2016-03-02 11:15:55 +0000

    8.54 bug include users_login_test.rb 47coment out!

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,30 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-    # テストユーザーがログインしていればtrueを返す
+  # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # 統合テスト内ではtrueを返す
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
