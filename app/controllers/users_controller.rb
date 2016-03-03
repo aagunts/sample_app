31500a4aaaa0b816745cb157bf69ce70fde6b2f7
kyamamoto <aagunts@gmail.com>
2016-03-03 00:38:10 +0000

    9.7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,9 +18,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功したときの処理
+    else
+      render 'edit'
+    end
+  end
+
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                     :password_confirmation)
+                                   :password_confirmation)
     end
 end
