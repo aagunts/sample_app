a828bd38998f477be8f12104c5cee3a8f8e224d6
kyamamoto <aagunts@gmail.com>
2016-03-06 08:42:10 +0000

    12.40 ALL err

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
 
   def setup
-    @user       = users(:michael)
+    @user = users(:michael)
     @other_user = users(:archer)
   end
 
@@ -55,5 +55,15 @@ class UsersControllerTest < ActionController::TestCase
       delete :destroy, id: @user
     end
     assert_redirected_to root_url
+  end
+  
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
   end
 end
